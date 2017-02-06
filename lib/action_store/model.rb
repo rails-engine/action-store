a65d12ea35bbe2a0711a3ed705dba707388c7ee4
Jason Lee <huacnlee@gmail.com>
2017-02-06 19:25:37 +0800

    remove a debug puts

--- lib/action_store/model.rb
+++ lib/action_store/model.rb
@@ -3,7 +3,7 @@ module ActionStore
     extend ActiveSupport::Concern
 
     included do
-      puts "Initalize ActionStore::Model"
+      # puts "Initalize ActionStore::Model"
       belongs_to :target, polymorphic: true
       belongs_to :user, class_name: ActionStore.config.user_class
     end
