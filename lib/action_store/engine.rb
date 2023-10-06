d6a957724a92cb3ee7bbc731dea86ffaaf9823ec
Jason Lee <huacnlee@gmail.com>
2023-10-06 13:37:41 +0800

    Fix engine load on Rails 7.1 #18

--- lib/action_store/engine.rb
+++ lib/action_store/engine.rb
@@ -3,10 +3,5 @@
 module ActionStore
   class Engine < ::Rails::Engine
     isolate_namespace ActionStore
-
-    initializer "action_store.init_action", after: :load_config_initializers do
-      # Ensure eager_load Action model to init methods
-      require "action"
-    end
   end
 end
